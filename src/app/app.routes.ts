4c17fdd3b298ed1f3cdc8707a4e4f0960f7d1d4a
Jonatan Florêncio <jonatanflorencio45@gmail.com>
2024-04-08 11:18:09 -0300

    alterações da aula passada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'cartas', pathMatch: 'full'},
+  {
+    path: 'cartas',
+    loadChildren:() => import('./cartas/cartas.module').then(m => m.CartasModule)
+  }
+];
---
+++ src/app/cartas/cartas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CartasRoutingModule } from './cartas-routing.module';
+import { CartaListagemComponent } from './carta-listagem/carta-listagem.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CartasRoutingModule
+  ]
+})
+export class CartasModule { }
